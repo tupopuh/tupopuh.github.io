4c6c58354cd5adf41e2fde0a1ff441736628ed1e
NOVOZHILOVA ANASTASIA (ALTUF399) <AVASIL12@neptune.dkcorp.net>
2018-04-04 10:46:30 +0300

    updated

--- 20180328/main.js
+++ 20180328/main.js
@@ -1,9 +1,9 @@
 //Ключ доступа к API
-var API_KEY = 'AIzaSyAKZcs_ievlf4SNciCWDpLbfKOX2H2xjKw';
+var API_KEY = 'kkk';
 
 //Идентификатор Google-таблицы (берется из адресной строки)
-var spreadsheetId = '1GxSbvZ5jJVH2gb-0BEERRdvAwHX_rUW-qYFaP8WJaZ0';
-
+var spreadsheetId = 'sss';
+                     
 var range = 'A1:B4'; // Диапазон ячеек, которые мы хотим поулчить из таблицы
 
 // Полная строка GET-запроса к API
